ba95bcd2575e040edf401125214702ad026218ee
dave gumba <dave_gumba@yahoo.com>
2020-08-23 14:10:31 -0400

    seems fine

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php 
 ob_start();
 session_start();
-$con = mysqli_connect("localhost", "root", "", "odytuts");
+$con = mysqli_connect("us-cdbr-east-02.cleardb.com", "b15ce807601a58", "d5368fc6", "odytuts");
 
 if(mysqli_connect_errno()) {
     echo "Failed to connect: " . mysqli_connect_errno();
