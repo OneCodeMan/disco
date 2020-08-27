65583048569ad5dfae18c46859184f712bb02df3
dave gumba <dave_gumba@yahoo.com>
2020-08-27 17:34:41 -0400

    done

--- config/config.php
+++ config/config.php
@@ -1,7 +1,7 @@
 <?php 
 ob_start();
 session_start();
-$con = mysqli_connect("us-cdbr-east-02.cleardb.com", "b15ce807601a58", "d5368fc6", "odytuts");
+$con = mysqli_connect("us-cdbr-east-02.cleardb.com", "b15ce807601a58", "d5368fc6", "heroku_34dac4a7fad0f34");
 
 if(mysqli_connect_errno()) {
     echo "Failed to connect: " . mysqli_connect_errno();
